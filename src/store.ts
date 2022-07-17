ad8b9a231dfcd1bac8ec086321f119852417d1a4
Atto <sharplancer021@gmail.com>
2022-07-17 17:27:17 +0300

    update home view

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,10 @@
 import { reactive } from 'vue'
 
 export const globalState = reactive({
-  count: 0,
+  name: '',
+  age: 50,
+  premium: 0,
+  package: 'Standard',
+  country: 'Hong Kong',
+  currency: 'HKD',
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
-  <HelloWorld msg="Vue 3, Vite, TypeScript & Tailwind CSS" />
+  <div class="prose max-w-none min-h-screen flex flex-col items-center justify-center">
+    <div class="mt-8 px-32 py-16 rounded-md bg-gray-100 flex flex-col items-center justify-center">
+      <h1>Hello There!</h1>
+      <div>Let's buy some insurance. It is going to take only a few steps.</div>
+      <router-link to="/personal">
+        <button class="mt-8 py-2 px-16 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800">
+          Start
+        </button>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import HelloWorld from '../components/HelloWorld.vue'
-
 export default defineComponent({
   components: {
-    HelloWorld,
   },
 })
 </script>
